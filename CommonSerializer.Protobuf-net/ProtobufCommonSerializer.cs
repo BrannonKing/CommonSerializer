a46443b9ee8e7932a306fa0c1c8fdc233e446de8
Brannon King <countprimes@gmail.com>
2015-10-21 23:58:19 -0600

    don't know what I'm doing yet on project organization with the new system

---
+++ CommonSerializer.Protobuf-net/ProtobufCommonSerializer.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using ProtoBuf;
+using ProtoBuf.Meta;
+
+namespace CommonSerializer.ProtobufNet
+{
+	public class ProtobufCommonSerializer: ICommonSerializer
+	{
+		private readonly RuntimeTypeModel _runtime;
+
+		public ProtobufCommonSerializer(MethodInfo classFactory = null)
+		{
+			_runtime = TypeModel.Create();
+			if (classFactory != null)
+				_runtime.SetDefaultFactory(classFactory);
+			RegisterSubtype<ISerializedContainer, ProtobufSerializedContainer>(1);
+		}
+
+		public ProtobufCommonSerializer(RuntimeTypeModel runtime)
+		{
+			_runtime = runtime;
+		}
+
+		public string Description
+		{
+			get
+			{
+				return "M. Gravell's Protocol Buffers Implementation";
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				return "Protobuf-net";
+			}
+		}
+
+		public bool StreamsUtf8
+		{
+			get
+			{
+				return false;
+			}
+		}
+
+		public T DeepClone<T>(T t)
+		{
+			return (T)_runtime.DeepClone(t);
+		}
+
+		public object Deserialize(string str, Type type)
+		{
+			using (var reader = new StringReader(str))
+				return Deserialize(reader, type);
+		}
+
+		public object Deserialize(TextReader reader, Type type)
+		{
+			var line = reader.ReadLine();
+			if (line == null)
+				return null;
+			var bytes = Convert.FromBase64String(line);
+			using (var ms = new MemoryStream(bytes, false))
+				return Deserialize(ms, type);
+		}
+
+		public object Deserialize(Stream stream, Type type)
+		{
+			return _runtime.DeserializeWithLengthPrefix(stream, null, type, PrefixStyle.Fixed32, 0);
+		}
+
+		public object Deserialize(ISerializedContainer container, Type type)
+		{
+			var psc = container as ProtobufSerializedContainer;
+			if (psc == null)
+				throw new ArgumentException("Invalid container type. Use the GenerateContainer method.");
+
+			return Deserialize(psc.Stream, type);
+		}
+
+		public T Deserialize<T>(Stream stream)
+		{
+			return (T)Deserialize(stream, typeof(T));
+		}
+
+		public T Deserialize<T>(string str)
+		{
+			return (T)Deserialize(str, typeof(T));
+		}
+
+		public T Deserialize<T>(TextReader reader)
+		{
+			return (T)Deserialize(reader, typeof(T));
+		}
+
+		public T Deserialize<T>(ISerializedContainer container)
+		{
+			return (T)Deserialize(container, typeof(T));
+		}
+
+		public ISerializedContainer GenerateContainer()
+		{
+			return new ProtobufSerializedContainer();
+		}
+
+		public void RegisterSubtype<TBase, TInheritor>(int fieldNumber)
+		{
+			_runtime[typeof(TBase)].AddSubType(fieldNumber, typeof(TInheritor));
+		}
+
+		public string Serialize<T>(T value)
+		{
+			return Serialize(value, typeof(T));
+		}
+
+		public string Serialize(object value, Type type)
+		{
+			var sb = new StringBuilder();
+			using (var stringWriter = new StringWriter(sb))
+				Serialize(stringWriter, value, type);
+			return sb.ToString();
+		}
+
+		public void Serialize<T>(TextWriter writer, T value)
+		{
+			Serialize(writer, value, typeof(T));
+		}
+
+		public void Serialize(TextWriter writer, object value, Type type)
+		{
+			using (var stream = new MemoryStream())
+			{
+				Serialize(stream, value, type);
+				stream.Flush();
+				var base64 = Convert.ToBase64String(stream.ToArray());
+				writer.Write(base64);
+			}
+		}
+
+		public void Serialize<T>(Stream stream, T value)
+		{
+			Serialize(stream, value, typeof(T));
+		}
+
+		public void Serialize(Stream stream, object value, Type type)
+		{
+			_runtime.SerializeWithLengthPrefix(stream, value, type, PrefixStyle.Fixed32, 0);
+		}
+
+		public void Serialize<T>(ISerializedContainer container, T value)
+		{
+			Serialize(container, value, typeof(T));
+		}
+
+        public void Serialize(ISerializedContainer container, object value, Type type)
+		{
+			var psc = container as ProtobufSerializedContainer;
+			if (psc == null)
+				throw new ArgumentException("Invalid container type. Use the GenerateContainer method.");
+
+			Serialize(psc.Stream, value, type);
+			psc.Count++;
+		}
+    }
+}
---
+++ CommonSerializer.Protobuf-net/ProtobufSerializedContainer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace CommonSerializer.ProtobufNet
+{
+	[ProtoBuf.ProtoContract(UseProtoMembersOnly = true)]
+	internal class ProtobufSerializedContainer : ISerializedContainer
+	{
+		private MemoryStream _stream = new MemoryStream();
+
+		public Stream Stream {  get { return _stream; } }
+
+		[ProtoBuf.ProtoMember(1)]
+		public byte[] Data
+		{
+			get { return _stream.ToArray(); } // need to flush first?
+			set { _stream = new MemoryStream(value, false); }
+		}
+
+		[ProtoBuf.ProtoMember(2)]
+		public int Count { get; internal set; }
+
+		public bool CanRead
+		{
+			get
+			{
+				return _stream.Position < _stream.Length;
+			}
+		}
+
+		public bool CanWrite
+		{
+			get
+			{
+				return _stream.Position >= _stream.Length;
+			}
+		}
+	}
+}
